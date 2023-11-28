6872db077ff0b93a2f8ef240c59d3ced633d1193
marcinmss <marciomsantoss@gmail.com>
2023-11-28 13:56:25 +0100

    Added function for doing the Whole TM Analysis including plotting the graphs

--- multifractal_analysis/trace_moment.py
+++ multifractal_analysis/trace_moment.py
@@ -1,8 +1,13 @@
 from math import log2
+from matplotlib import colormaps
 from typing import Tuple
 from numpy import empty, ndarray, mean, power
-from multifractal_analysis.general import upscale
+from matplotlib.axes import Axes
+from multifractal_analysis.general import is_power_of_2, upscale
 from multifractal_analysis.regression_solution import RegressionSolution
+from collections import namedtuple
+
+CMAP = colormaps["Set3"]
 
 """
 Function for calculating the moment of a field
@@ -45,3 +50,39 @@ def get_um_params_tm(field_1d: ndarray):
     c1 = (kf - k0) / (2 * h)
     alpha = (kf + k0) / (h**2 * c1)
     return alpha, c1
+
+
+"""
+"""
+TMAnalysis = namedtuple("TMAnalysis", ["alpha", "c1"])
+
+
+def tm_analysis(field: ndarray, ax: Axes | None) -> TMAnalysis:
+    assert is_power_of_2(field.shape[0]), "The field needs to be a power of 2"
+
+    if ax is not None:
+        for q in reversed((0.1, 0.5, 0.8, 1.01, 1.5, 2.0, 2.5)):
+            # Get the points and do the regression on them
+            x, y = get_trace_moment_points(field, q=q)
+            regression_solution = RegressionSolution(x, y)
+
+            # Set the axis apperence
+            ax.set_title("TM Analysis")
+            ax.set_ylabel(r"$\log _2 (TM_\lambda)$")
+            ax.set_xlabel(r"$\log _2 (\lambda)$")
+
+            # Plot the points of each analysis
+            legend = ", ".join(
+                (r"$q=$%.1f" % (q,), r"$r^2=$%.1f" % (regression_solution.r_square,))
+            )
+            color = CMAP(q / 2.5)
+            ax.scatter(x, y, color=color, label=legend)
+            ax.legend(prop={"size": 6}, framealpha=0.0)
+
+            # Plot the tendency line for each analysis
+            a, b = regression_solution.angular_coef, regression_solution.linear_coef
+            leftx, rightx = min(x), max(x)
+            ax.plot((leftx, rightx), (a * leftx + b, a * rightx + b), c=color)
+
+    alpha, c1 = get_um_params_tm(field)
+    return TMAnalysis(alpha, c1)
